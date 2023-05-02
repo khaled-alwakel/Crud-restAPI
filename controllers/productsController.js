871a04401c326cc590c08a4f521bb82c88854324
khaled-alwakel <khaledmofreh33@gmail.com>
2023-05-03 00:14:28 +0300

    implement update and delete

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const products = JSON.parse(
   fs.readFileSync(`${__dirname}/../data/products.json`)
 );
-exports.checkID = (req, res, next, val) => {
-  console.log(`product id is : ${val}`);
+exports.checkID = (req, res, next, value) => {
+  console.log(`product id is : ${value}`);
   const id = req.params.id * 1;
   if (id > products.length) {
     return res.status(404).json({
@@ -36,7 +36,7 @@ exports.getAllProducts = (req, res) => {
 
 exports.getProduct = (req, res) => {
   const id = req.params.id * 1;
-  const product = products.find((product) => product["Product ID"] === id);
+  const product = products.find((ele) => ele["Product ID"] === id);
   res.status(200).json({
     status: "success",
     data: { product },
@@ -47,9 +47,7 @@ exports.createProduct = (req, res) => {
   const newId = products.length + 1;
   const newProduct = Object.assign({ "Product ID": newId }, req.body);
 
-  const productExist = products.find(
-    (product) => product.Name === newProduct.Name
-  );
+  const productExist = products.find((ele) => ele.Name === newProduct.Name);
   if (productExist) return res.send("product already exist");
 
   products.push(newProduct);
@@ -66,9 +64,55 @@ exports.createProduct = (req, res) => {
 };
 
 exports.updateProduct = (req, res) => {
-  //TODO
+  const id = req.params.id * 1;
+  const existedProduct = products.find((ele) => ele["Product ID"] === id);
+
+  //new fields
+  const { Name, SKU, price, color } = req.body;
+
+  const updateField = (newValue, oldValue) => (!newValue ? oldValue : newValue);
+  const updatedProduct = {
+    "Product ID": id,
+    Name: updateField(Name, existedProduct.Name),
+    SKU: updateField(SKU, existedProduct.SKU),
+    price: updateField(price, existedProduct.price),
+    color: updateField(color, existedProduct.color),
+  };
+
+  // get old product index to replace it with new updatedProduct
+  const existedProductIndex = products.findIndex(
+    (ele) => ele["Product ID"] === existedProduct["Product ID"]
+  );
+  products.splice(existedProductIndex, 1, updatedProduct);
+
+  fs.writeFile(
+    `${__dirname}/../data/products.json`,
+    JSON.stringify(products),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: { updatedProduct },
+      });
+    }
+  );
 };
 
 exports.deleteProduct = (req, res) => {
-  //TODO
+  const id = req.params.id * 1;
+  const existedProduct = products.find((ele) => ele["Product ID"] === id);
+
+  const existedProductIndex = products.findIndex(
+    (ele) => ele["Product ID"] === existedProduct["Product ID"]
+  );
+  products.splice(existedProductIndex, 1);
+
+  fs.writeFile(
+    `${__dirname}/../data/products.json`,
+    JSON.stringify(products),
+    (err) => {
+      res.status(201).json({
+        status: "  Successfully Deleted",
+      });
+    }
+  );
 };
