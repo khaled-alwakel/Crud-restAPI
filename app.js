40e31172d403784c3577ab01547e35046735c80e
khaled-alwakel <khaledmofreh33@gmail.com>
2023-05-02 04:00:46 +0300

    read the json data and put some middle wares

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const fs = require("fs");
 const express = require("express");
-
 const app = express();
-module.exports = app;
+
+// middle wares
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("hello fromm the middleware 👋");
+  next();
+});
+
+const data = JSON.parse(fs.readFileSync(`${__dirname}/data/searchspring.json`));
+
+// useCase here : imagine that the route handler wants to know when exactly the request happens
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 app.get("/", (req, res) => {
   res.end("hello from server");
 });
+
+module.exports = app;
