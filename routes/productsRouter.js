0eb7c1e42a859c5b741bfa1209873fe451de8c3f
khaled-alwakel <khaledmofreh33@gmail.com>
2023-05-02 21:51:30 +0300

    added another middle wares to check the id before get product and update or delete

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,10 +2,14 @@ const express = require("express");
 const productsController = require("../controllers/productsController");
 const router = express.Router();
 
+//* param middleware
+// here we specify first the parameter that we actually want to search for, so basically the parameter for which this middleware is gonna run
+router.param("id", productsController.checkID);
+
 router
   .route("/")
   .get(productsController.getAllProducts)
-  .post(productsController.createProduct); // check body before create product
+  .post(productsController.checkBody, productsController.createProduct); // check body before create product
 
 router
   .route("/:id")
@@ -15,4 +19,5 @@ router
 
 module.exports = router;
 
-//  exports = router => Error, because it will be exported as an object , and app.use  in app.js is waiting for middleware function
+// if i make it like   exports = router. i will get Error
+// because it will be exported as an object , and app.use  in app.js is waiting for middleware function
